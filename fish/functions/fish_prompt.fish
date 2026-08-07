9fd0ace66857d7908290b6b41046a441a523078b
Andrew Lolotronop <lolotron300037200123@gmail.com>
2026-08-07 20:29:38 +0000

    the beginning of async prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,56 @@
-function fish_prompt --description 'Write out the prompt'
+# this is very much based on https://github.com/acomagu/fish-async-prompt
+
+status is-interactive
+or exit
+
+set -g __prompt_tmp (mktemp -d)
+set -g __prompt_gen 0
+set -g __prompt_branch ""
+
+
+function __prompt_async
+    set -g __prompt_gen (math $__prompt_gen + 1)
+
+    set -l gen $__prompt_gen
+    set -l dir $PWD
+    set -l file "$__prompt_tmp/$gen"
+
+    begin
+        git -C "$dir" branch --show-current 2>/dev/null >$file
+        kill -s SIGUSR1 $fish_pid 2>/dev/null
+    end &
+end
+
+
+function __prompt_repaint --on-signal SIGUSR1
+    set -l file "$__prompt_tmp/$__prompt_gen"
+
+    if test -f "$file"
+        set -g __prompt_branch (string collect <"$file")
+        commandline -f repaint >/dev/null 2>/dev/null
+    end
+end
+
+
+function __prompt_on_pwd --on-variable PWD
+    # The old branch is no longer relevant.
+    set -g __prompt_branch ""
+
+    __prompt_async
+end
+
+
+function __prompt_on_exec --on-event fish_postexec
+    __prompt_async
+end
+
+
+function __prompt_start --on-event fish_prompt
+    functions -e __prompt_start
+    __prompt_async
+end
+
+function fish_prompt
         set -l last_status $status
         set -l normal (set_color --reset)
         set -l status_color (set_color brcyan)
@@ -6,7 +58,6 @@ function fish_prompt --description 'Write out the prompt'
         set -l vcs_color (set_color brpurple)
         set -l prompt_status ""
 
-        # Since we display the prompt on a new line allow the directory names to be longer.
         set -q fish_prompt_pwd_dir_length
         or set -lx fish_prompt_pwd_dir_length 0
 
@@ -25,6 +76,14 @@ function fish_prompt --description 'Write out the prompt'
                 set prompt_status $status_color "[" $last_status "]" $normal
         end
 
-        echo -s \n $cwd_color (prompt_pwd)
+        echo -s -n \n $cwd_color (prompt_pwd)
+        if test -n "$__prompt_branch"
+                echo -s -n ' ' (set_color brpurple) '' $__prompt_branch
+        end
+        echo
         echo -n -s $status_color $suffix ' ' $normal
+end
+
+function __prompt_cleanup --on-event fish_exit
+    rm -rf "$__prompt_tmp"
 end
